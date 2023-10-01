41b22207e444c6c9f4ba779088e8cb8705f96e40
hritik001 <hritikrajkarn@gmail.com>
2023-10-01 16:32:08 +0545

    feat: export config for all import

---
+++ lib/export.dart
@@ -0,0 +1,10 @@
+library statemanagement;
+
+export 'config.dart';
+export 'models/obeservable_model.dart';
+export 'models/observable_action.dart';
+export 'mvvm_builder.widget.dart';
+export 'utils/extensions.dart';
+export 'utils/index.dart';
+export 'view_model.dart';
+export 'views/stateless.view.dart';
